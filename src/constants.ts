4269d2b97ec010546e043f50bda549a96cb88734
Litvinov Iurii <litvinov.yuriy1996@gmail.com>
2022-07-16 16:53:46 +0400

    fix: fixed linked db object

--- src/constants.ts
+++ src/constants.ts
@@ -4,18 +4,16 @@ export enum DataBaseEntity {
   ALBUMS = 'albums',
   TRACKS = 'tracks',
   FAVOURITES = 'favorites',
-}
+};
 
-const coreEntities = {
+export const initialDataBase = {
   [DataBaseEntity.ARTISTS]: {},
   [DataBaseEntity.ALBUMS]: {},
   [DataBaseEntity.TRACKS]: {},
-}
-
-export const initialDataBase = {
-  ...coreEntities,
   [DataBaseEntity.USERS]: {},
   [DataBaseEntity.FAVOURITES]: {
-    ...coreEntities,
+    [DataBaseEntity.ARTISTS]: {},
+    [DataBaseEntity.ALBUMS]: {},
+    [DataBaseEntity.TRACKS]: {},
   },
 }
