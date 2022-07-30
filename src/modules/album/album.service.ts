d9ff892309859f7533a95b4cb9ed3ea6023067f0
Litvinov Iurii <litvinov.yuriy1996@gmail.com>
2022-07-30 12:15:12 +0400

    refactor: Removed commented code

--- src/modules/album/album.service.ts
+++ src/modules/album/album.service.ts
@@ -1,22 +1,12 @@
 import { PrismaService } from './../../common/modules/prisma/prisma.service';
-import { CreateAlbumDto } from './dto/create-album.dto';
 import { Injectable } from '@nestjs/common';
 import { EntityService } from 'src/common/services/base-entity.service';
 import { DataBaseEntity } from 'src/constants';
 import { Album } from './entities/album.entity';
-import { UpdateAlbumDto } from './dto/update-album.dto';
 
 @Injectable()
 export class AlbumService extends EntityService<Album> {
   constructor(protected prisma: PrismaService) {
     super(prisma, DataBaseEntity.ALBUMS, Album);
   }
-  // override create(createAlbumDto: CreateAlbumDto): AlbumEntity {
-  //   this.checkRefers<CreateAlbumDto>([DataBaseEntity.ARTISTS], createAlbumDto);
-  //   return super.create(createAlbumDto);
-  // }
-  // override update(id: string, updateAlbumDto: UpdateAlbumDto): AlbumEntity {
-  //   this.checkRefers<UpdateAlbumDto>([DataBaseEntity.ARTISTS], updateAlbumDto);
-  //   return super.update(id, updateAlbumDto);
-  // }
 }
