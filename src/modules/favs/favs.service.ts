68f19189ee957750f2f445c15b8fdc586952d039
Litvinov Iurii <litvinov.yuriy1996@gmail.com>
2022-07-30 17:05:11 +0400

    feat: Partly implements Favs

--- src/modules/favs/favs.service.ts
+++ src/modules/favs/favs.service.ts
@@ -1,68 +1,61 @@
-import { FavsEntity } from './entities/fav.entity';
+import { PrismaService } from 'src/common/modules/prisma/prisma.service';
+import { NIL as NIL_UUID } from 'uuid';
+import { Favs } from './entities/fav.entity';
 import { Injectable, UnprocessableEntityException } from '@nestjs/common';
-import { InMemoryDBService } from 'src/in-memory-db';
 import { DataBaseEntity } from 'src/constants';
-import { ArtistEntity } from 'src/modules/artist/entities/artist.entity';
-import { AlbumEntity } from '../album/entities/album.entity';
-import { TrackEntity } from '../track/entities/track.entity';
-
-interface Dictionary {
-  [key: string]: string;
-}
-
-interface FavsDictionary {
-  [key: string]: Dictionary;
-}
+import { Artist } from 'src/modules/artist/entities/artist.entity';
+import { Album } from '../album/entities/album.entity';
+import { Track } from '../track/entities/track.entity';
 
 @Injectable()
 export class FavsService {
-  constructor(private readonly db: InMemoryDBService) {}
+  private defaultId: string = NIL_UUID;
 
-  findAll(): FavsEntity {
-    const { albums, artists, tracks } = this.db.get<{
-      albums: FavsDictionary;
-      artists: FavsDictionary;
-      tracks: FavsDictionary;
-    }>([DataBaseEntity.FAVOURITES]);
-
-    return {
-      albums: this.getEntitiesById<AlbumEntity>(albums, DataBaseEntity.ALBUMS),
-      artists: this.getEntitiesById<ArtistEntity>(
-        artists,
-        DataBaseEntity.ARTISTS,
-      ),
-      tracks: this.getEntitiesById<TrackEntity>(tracks, DataBaseEntity.TRACKS),
-    };
+  constructor(private readonly prisma: PrismaService) {
+    this.prisma.favs.create({
+      data: { id: this.defaultId },
+    });
   }
 
-  add(table: string, id: string): { message: string } {
-    if (this.checkEntityExist(table, id)) {
-      this.db.add([DataBaseEntity.FAVOURITES, table], { id });
+  async findAll(): Promise<any> {
+    try {
+      return await this.prisma.favs.findUniqueOrThrow({
+        where: { id: this.defaultId },
+        include: {
+          tracks: true,
+          artists: true,
+          albums: true,
+        },
+      });
+    } catch (e) {
+      this.prisma.handleErrors(e);
     }
-
-    return {
-      message: `${
-        table.charAt(0).toUpperCase() +
-        table.slice(0, table.length - 1).slice(1)
-      } with id ${id} successfully added to favorites`,
-    };
   }
 
-  remove(table: string, id: string): void {
-    this.db.delete([DataBaseEntity.FAVOURITES, table], id);
-  }
-
-  private checkEntityExist(table: string, id: string): any {
-    const entity = this.db.get([table, id]);
-    if (!entity) {
-      throw new UnprocessableEntityException(
-        `${table.slice(0, table.length - 1)} with id ${id} not exist`,
-      );
-    }
-    return !!entity;
-  }
-
-  private getEntitiesById<T>(entities: FavsDictionary, table: string): T[] {
-    return Object.values(entities).map(({ id }) => this.db.get<T>([table, id]));
-  }
+  // add(table: string, id: string): { message: string } {
+  //   if (this.checkEntityExist(table, id)) {
+  //     this.db.add([DataBaseEntity.FAVOURITES, table], { id });
+  //   }
+  //   return {
+  //     message: `${
+  //       table.charAt(0).toUpperCase() +
+  //       table.slice(0, table.length - 1).slice(1)
+  //     } with id ${id} successfully added to favorites`,
+  //   };
+  // }
+  // remove(table: string, id: string): void {
+  //   this.db.delete([DataBaseEntity.FAVOURITES, table], id);
+  // }
+  // private checkEntityExist(table: string, id: string): any {
+  //   const entity = this.db.get([table, id]);
+  //   if (!entity) {
+  //     throw new UnprocessableEntityException(
+  //       `${table.slice(0, table.length - 1)} with id ${id} not exist`,
+  //     );
+  //   }
+  //   return !!entity;
+  // }
+  // private getEntitiesById<T>(entities: FavsDictionary, table: string): T[] {
+  //   return Object.values(entities).map(({ id }) => this.db.get<T>([table, id]));
+  // }
 }
