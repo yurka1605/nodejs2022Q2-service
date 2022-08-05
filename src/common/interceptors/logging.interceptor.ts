a83a94595c3ff3a7860af3ee1a8842e856af8378
Litvinov Iurii <litvinov.yuriy1996@gmail.com>
2022-08-06 02:07:38 +0400

    feat: replace nest logger to custom logger

--- src/common/interceptors/logging.interceptor.ts
+++ src/common/interceptors/logging.interceptor.ts
@@ -2,17 +2,16 @@ import {
   CallHandler,
   ExecutionContext,
   Injectable,
-  Logger,
   NestInterceptor,
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
+import { AppLogger } from '../modules/logger/logger.service';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
-  logger: Logger;
-  constructor() {
-    this.logger = new Logger('App');
+  constructor(private logger: AppLogger) {
+    this.logger.setContext('APP');
   }
 
   intercept(ctx: ExecutionContext, next: CallHandler): Observable<any> {
--- src/common/modules/core.module.ts
+++ src/common/modules/core.module.ts
@@ -1,12 +1,10 @@
+import { LoggerModule } from './logger/logger.module';
 import { Module } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { LoggingInterceptor } from '../interceptors/logging.interceptor';
-import { TransformInterceptor } from '../interceptors/transform.interceptor';
 
 @Module({
-  providers: [
-    { provide: APP_INTERCEPTOR, useClass: TransformInterceptor },
-    { provide: APP_INTERCEPTOR, useClass: LoggingInterceptor },
-  ],
+  imports: [LoggerModule],
+  providers: [{ provide: APP_INTERCEPTOR, useClass: LoggingInterceptor }],
 })
 export class CoreModule {}
